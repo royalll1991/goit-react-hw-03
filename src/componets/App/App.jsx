f66c926101bd0f99283ade90f12b21b286c26a5c
Your Name <kadry.nukr@ukr.net>
2024-03-06 02:27:34 +0200

    Changes

--- src/componets/App/App.jsx
+++ src/componets/App/App.jsx
@@ -1,27 +1,25 @@
-// import { useEffect, useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import ContactForm from "../ContactForm/ContactForm";
-import SearchBox  from "../SearchBox/SearchBox";
+import SearchBox from "../SearchBox/SearchBox";
 import ContactList from "../ContactList/ContactList";
 
-
-
 function App() {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
 
- 
-//  useEffect(() => {
-//   window.localStorage.setItem( "all-feedback", JSON.stringify(feedback)), [feedback];
-//  })
   return (
-    
-     <div>
-  <h1>Phonebook</h1>
-  <ContactForm />
-  <SearchBox />
-  <ContactList />
-</div>
-  
-  )
+    <div>
+      <h1 className="text">Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList contacts={contacts} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/componets/ContactList/ContactList.jsx
+++ src/componets/ContactList/ContactList.jsx
@@ -1,15 +1,37 @@
+import { FaUser, FaPhone } from 'react-icons/fa';
+import css from './ContactList.module.css';
 
-
-const ContactList = () => {
-    
-  
-  
-
-    return(
-<>
-
-</>
-    )
+const Contact = ({ name, number }) => {
+  return (
+    <div className={css.box}>
+      <div className={css.buttonBox}>
+        <div className={css.userBox}>
+        <FaUser className={css.userImg}/>      
+      <p className={css.text}>{name}</p>
+      </div>
+      <div className={css.userBox}> 
+        <FaPhone className={css.userImg}/>      
+      <p className={css.text}>{number}</p>
+      </div>
+      </div>
+      <button className={css.buttonStyle}>Delete</button>
+    </div>
+  );
 };
+
+
+const ContactList  = ({contacts}) => {
+    return (<ul className={css.list}>
+        {contacts.map((contact) => (
+                <li  key={contact.id} >
+                    <Contact 
+                        name={contact.name}
+                        number={contact.number}
+                        />
+                </li>
+            ))}
+        
+    </ul>
+    );}
 
 export default ContactList;

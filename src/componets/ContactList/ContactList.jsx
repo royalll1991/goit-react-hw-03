6825d8f73925b7c035ef80e6aff154c4161c1b8f
Your Name <kadry.nukr@ukr.net>
2024-03-08 01:35:54 +0200

    done

--- src/componets/ContactList/ContactList.jsx
+++ src/componets/ContactList/ContactList.jsx
@@ -1,7 +1,10 @@
 import { FaUser, FaPhone } from 'react-icons/fa';
 import css from './ContactList.module.css';
 
-const Contact = ({ name, number }) => {
+const Contact = ({ id, name, number, onDelete}) => {
+  const handleDelete = () => {
+    onDelete(id);
+  };
   return (
     <div className={css.box}>
       <div className={css.buttonBox}>
@@ -14,19 +17,23 @@ const Contact = ({ name, number }) => {
       <p className={css.text}>{number}</p>
       </div>
       </div>
-      <button className={css.buttonStyle}>Delete</button>
+      <button className={css.buttonStyle} onClick={handleDelete}>Delete</button>
     </div>
   );
 };
 
 
-const ContactList  = ({contacts}) => {
-    return (<ul className={css.list}>
-        {contacts.map((contact) => (
+const ContactList  = ({contacts, filteredContacts, onDelete}) => {
+const contactsRender = filteredContacts || contacts;
+
+    return ( <ul className={css.list}>
+        {contactsRender.map((contact) => (
                 <li  key={contact.id} >
                     <Contact 
                         name={contact.name}
                         number={contact.number}
+                        id = {contact.id}
+                        onDelete = {onDelete}
                         />
                 </li>
             ))}
